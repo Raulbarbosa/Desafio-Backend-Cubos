c7e2ac10af634fc96414451f3c06df8a8b50c75d
Raul Barbosa <barbosamartins94@gmail.com>
2022-08-19 15:14:53 -0300

    Criação do endpoint login

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { createUser } = require('./controllers/users');
+const { login } = require('./controllers/login');
 
 const routes = express();
 
 routes.post('/users', createUser);
+routes.post('/login', login);
 
 module.exports = routes;
